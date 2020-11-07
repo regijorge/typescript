75a2a2418d30b855e2040d2d3f24bc5e18f6a74f
Jorge Souza <regi.jorge.s@gmail.com>
2020-11-07 14:45:18 -0300

    Project Sort: LinkedList

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,11 +1,17 @@
 import { Sorter } from './Sorter'
 import { NumbersCollection } from './NumbersCollection'
 import { CharactersCollection } from './CharactersCollection'
+import { LinkedList } from './LinkedList'
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0])
 const charactersCollection = new CharactersCollection('XaaaBc4Dx')
 
-const sorter = new Sorter(charactersCollection)
-sorter.sort()
+const linkedList = new LinkedList()
+linkedList.add(500)
+linkedList.add(-10)
+linkedList.add(-3)
+linkedList.add(4)
 
-console.log(sorter.collection.data)
+const sorter = new Sorter(linkedList)
+sorter.sort()
+linkedList.print()
