07e28a56c81667f41a09949583d58f8272d04e10
Jorge Souza <regi.jorge.s@gmail.com>
2020-11-07 18:02:27 -0300

    Stats: Parse

--- stats/src/CsvFileReader.ts
+++ stats/src/CsvFileReader.ts
@@ -1,4 +1,6 @@
 import fs from 'fs'
+import { dateStringToDate } from './utils'
+import { MatchResult } from './MatchResult'
 
 export class CsvFileReader {
   data: string[][] = []
@@ -10,6 +12,16 @@ export class CsvFileReader {
       encoding: 'utf-8'
     }).split('\n').map((row: string): string[] => {
       return row.split(',')
+    }).map((row: string[]): any => {
+      return [
+        dateStringToDate(row[0]),
+        row[1],
+        row[2],
+        parseInt(row[3]),
+        parseInt(row[4]),
+        row[5] as MatchResult,
+        row[6]
+      ]
     })
   }
 }
