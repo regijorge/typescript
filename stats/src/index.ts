24b6bebb887300f812999c7d2acc9fd2cadbce12
Jorge Souza <regi.jorge.s@gmail.com>
2020-11-08 12:25:22 -0300

    Stats: Static method for Match Reader

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,11 +1,9 @@
-import { CsvFileReader } from './CsvFileReader'
 import { MatchReader } from './MatchReader'
 import { ConsoleReport } from './reportTargets/ConsoleReport'
 import { WinsAnalysis } from './analysers/WinsAnalysis'
 import { Summary } from './Summary'
 
-const csvFileReader = new CsvFileReader('football.csv')
-const matchReader = new MatchReader(csvFileReader)
+const matchReader = MatchReader.fromCsv('football.csv')
 matchReader.load()
 
 const summary = new Summary(
