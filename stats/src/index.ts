31ba8587ce2db2f08f0980f021a06b704221f904
Jorge Souza <regi.jorge.s@gmail.com>
2020-11-08 10:33:25 -0300

    Stats: Composition

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,11 +1,13 @@
-import { MatchFileReader } from './MatchFileReader'
+import { CsvFileReader } from './CsvFileReader'
+import { MatchReader } from './MatchReader'
 import { MatchResult } from './MatchResult'
 
-const reader = new MatchFileReader('football.csv')
-reader.read()
+const csvFileReader = new CsvFileReader('football.csv')
+const matchReader = new MatchReader(csvFileReader)
+matchReader.load()
 
 let manUnitedWins = 0
-for (let match of reader.data) {
+for (let match of matchReader.matches) {
   if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUnitedWins++
   } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
