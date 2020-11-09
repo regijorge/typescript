4c0cab0921a1a355ae7495606295a866d847cd95
Jorge Souza <regi.jorge.s@gmail.com>
2020-11-09 09:41:15 -0300

    Web: Updating user props

--- web/src/index.ts
+++ web/src/index.ts
@@ -2,5 +2,7 @@ import { User } from './models/User'
 
 const user = new User({ name: 'Jorginho', age: 28 })
 
+user.set({ name: 'Jorjão', age: 29 })
+
 console.log('name', user.get('name'))
 console.log('age', user.get('age'))
