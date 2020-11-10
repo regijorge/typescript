eaa1658fe91373badb1ade6f3a3af7df2ab9436b
Jorge Souza <regi.jorge.s@gmail.com>
2020-11-09 22:32:51 -0300

    Web: Event to user

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,4 +1,7 @@
 import { User } from './models/User'
 
 const user = new User({ name: 'new record', age: 99 })
-user.save()
+user.events.on('change', () => {
+  console.log('eita')
+})
+user.events.trigger('change')
