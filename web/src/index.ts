35ba006e3d26845c687da3486d6cea6e93ae2a42
Jorge Souza <regi.jorge.s@gmail.com>
2020-11-09 21:53:27 -0300

    Web: Fetch user

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,18 +1,4 @@
 import { User } from './models/User'
 
-const user = new User({ name: 'Jorginho', age: 28 })
-
-user.on('change', () => {
-  console.log('Change number 1')
-})
-
-user.on('change', () => {
-  console.log('Change number 2')
-})
-
-user.on('save', () => {
-  console.log('Save something')
-})
-
-user.trigger('change')
-user.trigger('save')
+const user = new User({ id: 1 })
+user.fetch()
