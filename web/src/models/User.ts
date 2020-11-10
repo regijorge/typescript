05f6d08a5e21022432a082dfce38e2e932489e09
Jorge Souza <regi.jorge.s@gmail.com>
2020-11-10 20:40:03 -0300

    Web: Reintegrating Attributes

--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -1,5 +1,6 @@
 import { Eventing } from './Eventing'
 import { Sync } from './Sync'
+import { Attributes } from './Attributes'
 
 export interface UserProps {
   id?: number
@@ -12,4 +13,9 @@ const rootUrl = 'http://localhost:3000/users'
 export class User {
   events: Eventing = new Eventing()
   sync: Sync<UserProps> = new Sync<UserProps>(rootUrl)
+  attributes: Attributes<UserProps>
+
+  constructor(attrs: UserProps) {
+    this.attributes = new Attributes<UserProps>(attrs)
+  }
 }
