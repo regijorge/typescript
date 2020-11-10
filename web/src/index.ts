6594aee4d771d7456eb26811c9e9d52f4261bacd
Jorge Souza <regi.jorge.s@gmail.com>
2020-11-09 21:04:53 -0300

    Web: Register events

--- web/src/index.ts
+++ web/src/index.ts
@@ -2,7 +2,6 @@ import { User } from './models/User'
 
 const user = new User({ name: 'Jorginho', age: 28 })
 
-user.set({ name: 'Jorjão', age: 29 })
+user.on('change', () => {})
 
-console.log('name', user.get('name'))
-console.log('age', user.get('age'))
+console.log(user)
