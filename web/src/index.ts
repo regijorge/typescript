5bd3ae2b1e1fa584418891b890ee6c1b66204a74
Jorge Souza <regi.jorge.s@gmail.com>
2020-11-11 10:03:28 -0300

    Web: Saving Data

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,8 +1,8 @@
 import { User } from './models/User'
 
-const user = new User({ id: 1 })
+const user = new User({ name: 'Jorgito': age: 18 })
 
 const on = user.on
-on('change', () => console.log(user))
+on('save', () => console.log('User was saved'))
 
-user.fetch()
+user.save()
