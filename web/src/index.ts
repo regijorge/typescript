431b9d15eebbb9a809fa54faff4b144e9e25aa24
Jorge Souza <regi.jorge.s@gmail.com>
2020-11-11 20:39:32 -0300

    Web: Model Wrapup

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,6 +1,6 @@
 import { User } from './models/User'
 
-const user = new User({ name: 'Jorgito': age: 18 })
+const user = User.buildUser({ name: 'Jorginho': age: 28 })
 
 const on = user.on
 on('save', () => console.log('User was saved'))
