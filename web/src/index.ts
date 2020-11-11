fe5c01d9330d1e3ec10e0bd56b77937eaa64c33f
Jorge Souza <regi.jorge.s@gmail.com>
2020-11-11 09:53:22 -0300

    Web: Fetching user data

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,12 +1,8 @@
 import { User } from './models/User'
 
-const user = new User({ name: 'new record', age: 99 })
-
-console.log(user.get('name'))
+const user = new User({ id: 1 })
 
 const on = user.on
-const trigger = user.trigger
+on('change', () => console.log(user))
 
-on('change', () => console.log('user has been changed'))
-
-user.set({ name: 'a new name'})
+user.fetch()
