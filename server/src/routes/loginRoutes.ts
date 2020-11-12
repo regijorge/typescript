5984f226e13e84851f8ced3f4c27cbfac33b6138
Jorge Souza <regi.jorge.s@gmail.com>
2020-11-12 09:08:39 -0300

    Server: Parsing form body

--- server/src/routes/loginRoutes.ts
+++ server/src/routes/loginRoutes.ts
@@ -2,8 +2,25 @@ import { Router, Request, Response } from 'express'
 
 const router = Router()
 
-router.get('/', (req: Request, res: Response) => {
-  res.send('hi there')
+router.get('/login', (req: Request, res: Response) => {
+  res.send(`
+    <form method="POST">
+      <div>
+        <label>Email</label>
+        <input type="text" name="email" />
+      </div>
+      <div>
+        <label>Password</label>
+        <input type="password" name="password" />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  `)
+})
+
+router.post('/login', (req: Request, res: Response) => {
+  const  { email, password } = req.body
+  res.send(email + password)
 })
 
 export { router }
