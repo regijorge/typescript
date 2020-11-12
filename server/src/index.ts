a27a0231f48ceae29980a19623c08b2426990737
Jorge Souza <regi.jorge.s@gmail.com>
2020-11-12 08:57:50 -0300

    Server: Express Router

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,9 @@
-import express, { Request, Response } from 'express'
+import express from 'express'
+import { router } from './routes/loginRoutes'
 
 const app = express()
 
-app.get('/', (req: Request, res: Response) => {
-  res.send(`
-    <h1>Hi there</h1>
-  `)
-})
+app.use(router)
 
 app.listen(3000, () => {
   console.log('Listening to http://localhost:3000')
